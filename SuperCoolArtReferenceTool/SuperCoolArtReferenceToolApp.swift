5555cd689d10dd64602e4a42c8aaa8e428667f3d
al3913 <al3913@g.rit.edu>
2026-02-16 14:41:59 -0500

    Initial Commit

---
+++ SuperCoolArtReferenceTool/SuperCoolArtReferenceToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperCoolArtReferenceToolApp.swift
+//  SuperCoolArtReferenceTool
+//
+//  Created by andy lin on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SuperCoolArtReferenceToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
